6c11012dd96ed306fe5e2258dfa0f01123445471
Em Karisch <emii.khaos@gmail.com>
2026-07-10 09:23:22 +0200

    fix: force settings to default values

---
+++ settings-final-fixes.lua
@@ -0,0 +1,8 @@
+local Settings = require("__cf-lib__.settings.Settings")
+
+Settings.forceDefault("s6x-spoilage")
+Settings.forceDefault("s6x-container-recipe")
+Settings.forceDefault("s6x-biochamber-allow")
+Settings.forceDefault("s6x-biochamber-productivity")
+Settings.forceDefault("s6x-freeze-time")
+Settings.forceDefault("s6x-freezer-efficiency")
